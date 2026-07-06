ed5b336ae2aebd4098a60a3b959c303aa17094cb
Alvino Junior <alvinojrdev@gmail.com>
2026-07-06 20:45:46 -0300

    criando arquivos de ambiente

--- src/main/java/com/clinica/api/config/CorsConfig.java
+++ src/main/java/com/clinica/api/config/CorsConfig.java
@@ -14,6 +14,7 @@ public class CorsConfig {
 
         CorsConfiguration config = new CorsConfiguration();
 
+        config.addAllowedOrigin("http://localhost:4200");
         config.addAllowedOrigin("https://clinica-tanandra.onrender.com");
         config.addAllowedHeader("*");
         config.addAllowedMethod("*");
